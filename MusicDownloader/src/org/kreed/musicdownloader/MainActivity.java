d5a02f359173876763ee26e4a764a234cef72f20
q1bot <q1bot@crunchbang>
2014-07-21 14:37:09 +0300

    Add search to MusicDownloader on Fragment

--- MusicDownloader/src/org/kreed/musicdownloader/MainActivity.java
+++ MusicDownloader/src/org/kreed/musicdownloader/MainActivity.java
@@ -1,40 +1,31 @@
 package org.kreed.musicdownloader;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.TextWatcher;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.RadioGroup;
 
-public class MainActivity extends Activity implements
+public class MainActivity extends FragmentActivity implements
 		DialogInterface.OnClickListener, DialogInterface.OnDismissListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		ViewGroup layout = (ViewGroup) findViewById(R.id.activity_main);
-		View searchView = SearchTab.getInstanceView(getLayoutInflater(), this);
-		layout.addView(searchView);
+		FragmentManager manager = getSupportFragmentManager();
+		manager.beginTransaction().replace(R.id.activity_main, new SearchFragment()).commit();
+//		ViewGroup layout = (ViewGroup) findViewById(R.id.activity_main);
+//		View searchView = SearchTab.getInstanceView(getLayoutInflater(), this);
+//		layout.addView(searchView);
 	}
 
 	@Override
 	public void onDismiss(DialogInterface dialog) {
 		// TODO Auto-generated method stub
-//		ListView list = ((AlertDialog)dialog).getListView();
-//		// subtract 1 for header
-//		int which = list.getCheckedItemPosition() - 1;
-//
-//		RadioGroup group = (RadioGroup)list.findViewById(R.id.sort_direction);
-//		if (group.getCheckedRadioButtonId() == R.id.descending)
-//			which = ~which;
-
-//		mPagerAdapter.setSortMode(which);
 	}
 
 	@Override
@@ -43,6 +34,7 @@ public class MainActivity extends Activity implements
 		dialog.dismiss();
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Override
 	protected Dialog onCreateDialog(int id, Bundle args) {
 		if (id == SearchTab.STREAM_DIALOG_ID) {
