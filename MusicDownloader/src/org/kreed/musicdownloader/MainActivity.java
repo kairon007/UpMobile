97249551dcce977e7e90c0fff464dbc03e4d5b6d
Александр <@Alex-PC>
2014-07-21 10:30:56 +0300

    created project "MusicDownloader"

---
+++ MusicDownloader/src/org/kreed/musicdownloader/MainActivity.java
@@ -0,0 +1,22 @@
+package org.kreed.musicdownloader;
+
+import com.example.simpleapp.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
