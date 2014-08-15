a2f5e5e6d1656ddd5ecfc9bf7c4e019c93695445
Aleksandr <Aleksandr@192.168.8.100>
2014-08-15 15:44:10 +0300

    update engine Vmusice

--- MusicDownloader/src/org/kreed/musicdownloader/engines/SearchVmusice.java
+++ MusicDownloader/src/org/kreed/musicdownloader/engines/SearchVmusice.java
@@ -1,12 +1,19 @@
 package org.kreed.musicdownloader.engines;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Comment;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.kreed.musicdownloader.song.RemoteSong;
+
+
+
 
 
 import android.util.Log;
@@ -18,6 +25,7 @@ public class SearchVmusice extends BaseSearchTask {
 	}
 	
 	private static String VMUSIC_URL = "http://vmusice.net/mp3/";
+	private static final DateFormat isoDateFormat = new SimpleDateFormat("mm:ss", Locale.ENGLISH);
 	
 	@Override
 	protected Void doInBackground(Void... params) {
@@ -25,14 +33,22 @@ public class SearchVmusice extends BaseSearchTask {
 		try {
 			// Connect to the web site
 			Document document = Jsoup.connect(url).get();
-			for (Element songTag : document.select("div.main2")) {
+			for (Element songTag : document.select("li.x-track")) {
 				try {
-					Element victim = songTag.select("div.clear").first();
-					victim.html(((Comment)songTag.select("a.play").first().nextSibling().nextSibling()).getData());
-					String author = victim.select("span#autor").first().text();  
-					String name = victim.select("span#title").first().text();  
+					Elements authorElements = songTag.select("strong");
+					String author = authorElements.text();
+					Elements nameElements = songTag.getElementsByClass("title");
+					String name = nameElements.toString();
+					String startString = "</strong> – ";
+					int start = name.indexOf(startString);
+					int end = name.indexOf("</span>");
+					name = name.substring(start + startString.length(), end);
 					String downloadUrl = songTag.select("a.download").first().attr("href");
-					addSong(new RemoteSong(downloadUrl).setTitle(name).setArtistName(author));
+					String duration = songTag.select("em").text().replace("(", "").replace(")", "");
+					if (duration.length() == 4) {
+						duration = "0" + duration;
+					}
+					addSong(new RemoteSong(downloadUrl).setTitle(name).setArtistName(author).setDuration(isoDateFormat.parse(duration).getTime()));
 				} catch (Exception e) {
 					Log.e(getClass().getSimpleName(), "Error parsing song: "+songTag, e);
 				}
