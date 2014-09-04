d22969ff27e0922bb2fa43df86ca93150fba13cf
razer89 <razer0312@gmail.com>
2014-09-04 16:03:23 +0300

    Vanilla visual feedback fixed

--- vanilla-master/src/org/kreed/vanilla/LibraryActivity.java
+++ vanilla-master/src/org/kreed/vanilla/LibraryActivity.java
@@ -1315,9 +1315,9 @@ public class LibraryActivity
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
-		if ("AppTheme.Black".equals(Util.getThemeName(this))) {
-			setMenuBackgroundBlack();
-		}
+//		if ("AppTheme.Black".equals(Util.getThemeName(this))) {
+//			setMenuBackgroundBlack();
+//		}
 		Log.d("log", "onCreateOptionsMenu");
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 			MenuItem controls = menu.add(null);
@@ -1335,27 +1335,27 @@ public class LibraryActivity
 		return super.onCreateOptionsMenu(menu);
 	}
 	
-	protected void setMenuBackgroundBlack() {
-		Log.d("log", "setMenuBackgroundBlack");
-		getLayoutInflater().setFactory(new Factory() {
-			public View onCreateView(String name, Context context,
-					AttributeSet attrs) {
-					try {
-						LayoutInflater f = getLayoutInflater();
-						final View view = f.createView(name, null, attrs);
-						new Handler().post(new Runnable() {
-							public void run() {
-								view.setBackgroundResource(R.color.window_background_black);
-							}
-						});
-						return view;
-					} catch (InflateException e) {
-					} catch (ClassNotFoundException e) {
-					}
-				return null;
-			}
-		});
-	}
+//	protected void setMenuBackgroundBlack() {
+//		Log.d("log", "setMenuBackgroundBlack");
+//		getLayoutInflater().setFactory(new Factory() {
+//			public View onCreateView(String name, Context context,
+//					AttributeSet attrs) {
+//					try {
+//						LayoutInflater f = getLayoutInflater();
+//						final View view = f.createView(name, null, attrs);
+//						new Handler().post(new Runnable() {
+//							public void run() {
+//								view.setBackgroundResource(R.color.window_background_black);
+//							}
+//						});
+//						return view;
+//					} catch (InflateException e) {
+//					} catch (ClassNotFoundException e) {
+//					}
+//				return null;
+//			}
+//		});
+//	}
 
 	@Override
 	public boolean onPrepareOptionsMenu(Menu menu)
