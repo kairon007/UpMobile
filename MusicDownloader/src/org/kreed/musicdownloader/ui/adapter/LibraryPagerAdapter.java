8a762e182de2be087306050130854f36c2ae87cd
razer89 <razer0312@gmail.com>
2014-09-30 17:12:21 +0300

    Fix bug - Library show songs that not be downloaded

--- MusicDownloader/src/org/kreed/musicdownloader/ui/adapter/LibraryPagerAdapter.java
+++ MusicDownloader/src/org/kreed/musicdownloader/ui/adapter/LibraryPagerAdapter.java
@@ -52,6 +52,7 @@ import android.os.Parcelable;
 import android.preference.PreferenceManager;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -313,7 +314,9 @@ public class LibraryPagerAdapter extends PagerAdapter implements Handler.Callbac
 				String string = files[i].getName();
 				if (string.endsWith(".mp3")) {
 					MusicData musicData = new MusicData(files[i]);
-					adapterLibrary.add(musicData);
+					if (null != musicData.getFileUri()) {
+						adapterLibrary.add(musicData);
+					}
 				}
 			}
 			return null;
