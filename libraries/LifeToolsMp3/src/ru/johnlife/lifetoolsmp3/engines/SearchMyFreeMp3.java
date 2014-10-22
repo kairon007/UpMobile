6a04c9c904133e83e59e7f0cffbd1ec8d77a29ed
Klepcha Siargej <sklep4a@gmail.com>
2014-10-23 00:59:49 +0300

    fix SearchMyFreeMp3 engines and fix #267 and #256

--- libraries/LifeToolsMp3/src/ru/johnlife/lifetoolsmp3/engines/SearchMyFreeMp3.java
+++ libraries/LifeToolsMp3/src/ru/johnlife/lifetoolsmp3/engines/SearchMyFreeMp3.java
@@ -6,6 +6,7 @@ import java.net.URLEncoder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import ru.johnlife.lifetoolsmp3.song.RemoteSong;
 
@@ -24,16 +25,17 @@ public class SearchMyFreeMp3 extends SearchWithPages {
 	protected Void doInBackground(Void... params) {
 		try {
 			Document document = Jsoup.connect(String.format(URL, URLEncoder.encode(getSongName(), "UTF-8"))).get();
-			Element playlist = document.getElementById("content")
-						.getElementsByClass("white").first()
-						.getElementsByClass("inner").first()
-						.getElementsByClass("playlist").first();
+			Element playlist = document.select("ul.playlist").first();
+			if (null == playlist || null == playlist.children()) {
+				Log.d("log", "request is invalid");
+				return null;
+			}
 			for (Element element : playlist.children()) {
 				Element info = element.getElementsByTag("a").first();
 				String[] src = info.text().split(" - ");
 				String artist = src[0];
 				String title;
-				if (src[1].contains("mp3")) {
+				if (src[1].contains(".mp3")) {
 					title = src[1].split(".mp")[0];
 				} else {
 					title = src[1];
