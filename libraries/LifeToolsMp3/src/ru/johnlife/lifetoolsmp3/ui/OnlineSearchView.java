108fde20c35dc1d0f4a5cd0a8a54ff7829f4d8a2
Aleksandr <Aleksandr@192.168.56.1>
2014-11-10 10:36:51 +0300

    Fix #366

--- libraries/LifeToolsMp3/src/ru/johnlife/lifetoolsmp3/ui/OnlineSearchView.java
+++ libraries/LifeToolsMp3/src/ru/johnlife/lifetoolsmp3/ui/OnlineSearchView.java
@@ -229,8 +229,9 @@ public abstract class OnlineSearchView extends View {
 
 			if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB) {
 				adapter = new ArrayAdapter<String>(getContext(), R.layout.item_of_engine, list);
-			} else
+			} else {
 				adapter = new CustomSpinnerAdapter(getContext(), 0, list);
+			}
 			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 			spEnginesChoiser.setAdapter(adapter);
 			if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB) {
@@ -272,11 +273,6 @@ public abstract class OnlineSearchView extends View {
 					} else if (keyEngines.equals(getTitleSearchEngine8())) {
 						editor.putString(SPREF_CURRENT_ENGINES, getTitleSearchEngine8());
 						keyEngines = getTitleSearchEngine8();
-					}
-					if (Util.getThemeName(getContext()).equals("AppTheme.White")) {
-						((TextView) parent.getChildAt(0)).setTextColor(getContext().getResources().getColor(android.R.color.black));
-					} else {
-						((TextView) parent.getChildAt(0)).setTextColor(getContext().getResources().getColor(android.R.color.white));
 					}
 					editor.commit();
 				}
