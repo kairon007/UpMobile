d998cc08db9a0ec64d48e6b68a94e1f3dc5364ed
Daniil Pastuhov <daniil.pastuhov@gmail.com>
2014-11-14 14:14:09 +0300

    fixed diffrent size and colors of icon.

--- vanilla-master/src/org/kreed/vanilla/Settings.java
+++ vanilla-master/src/org/kreed/vanilla/Settings.java
@@ -32,7 +32,7 @@ import android.preference.PreferenceManager;
 public class Settings implements BaseSettings {
 	
 	private static final String[][] SEARCH_ENGINES = new String[][] { 
-		{"SearchVmusice", "1"}, 
+		//{"SearchVmusice", "1"}, 
 		{"SearchSoArdIyyin", "3"},
 		{"SearchPleer", "3"},
 		{"SearchMyFreeMp3", "1"},
