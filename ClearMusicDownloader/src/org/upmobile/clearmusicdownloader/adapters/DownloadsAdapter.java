9c07ff47070398a70f39e1112549a8b0e7ae960c
Aleksandr <Aleksandr@192.168.56.1>
2014-12-01 17:11:29 +0300

    remove current item item

--- ClearMusicDownloader/src/org/upmobile/clearmusicdownloader/adapters/DownloadsAdapter.java
+++ ClearMusicDownloader/src/org/upmobile/clearmusicdownloader/adapters/DownloadsAdapter.java
@@ -6,19 +6,24 @@ import ru.johnlife.lifetoolsmp3.Util;
 import ru.johnlife.lifetoolsmp3.song.Song;
 import android.content.Context;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.special.utils.UICircularImage;
 
 public class DownloadsAdapter extends BaseAdapter {
-	
+
 	private class DownloadsViewHolder extends ViewHolder<Song> {
 		TextView title;
 		TextView artist;
 		TextView duration;
+		TextView cancel;
+		ProgressBar progress;
 		UICircularImage image;
 
 		private View v;
+		private Song item;
 
 		public DownloadsViewHolder(View v) {
 			this.v = v;
@@ -26,10 +31,20 @@ public class DownloadsAdapter extends BaseAdapter {
 			artist = (TextView) v.findViewById(R.id.item_description);
 			image = (UICircularImage) v.findViewById(R.id.item_image);
 			duration = (TextView) v.findViewById(R.id.item_duration);
+			progress = (ProgressBar) v.findViewById(R.id.item_progress);
+			cancel = (TextView) v.findViewById(R.id.hidden_view);
+			cancel.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					remove(item);
+				}
+			});
 		}
 
 		@Override
 		protected void hold(Song item) {
+			this.item = item;
 			title.setText(item.title);
 			artist.setText(item.artist);
 			image.setImageResource(R.drawable.fallback_cover);
@@ -45,5 +60,4 @@ public class DownloadsAdapter extends BaseAdapter {
 	protected ViewHolder<Song> createViewHolder(View v) {
 		return new DownloadsViewHolder(v);
 	}
-
 }
