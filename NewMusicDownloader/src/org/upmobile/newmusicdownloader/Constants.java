4ca1800a0ae14f8a4a4961e8ad5f52523d8fc2e8
Aleksandr <Aleksandr@192.168.56.1>
2015-01-05 13:47:53 +0300

    hotfix

--- NewMusicDownloader/src/org/upmobile/newmusicdownloader/Constants.java
+++ NewMusicDownloader/src/org/upmobile/newmusicdownloader/Constants.java
@@ -5,6 +5,6 @@ import java.io.File;
 public class Constants {
 	public static final String KEY_SELECTED_SONG = "key.selected.song";
 	public static final String KEY_SELECTED_POSITION = "key.selected.position";
-	public static final String DIRECTORY_PREFIX = File.separator + "ClearMusicDownloader" + File.separator;
+	public static final String DIRECTORY_PREFIX = File.separator + "NewMusicDownloader" + File.separator;
 
 }
