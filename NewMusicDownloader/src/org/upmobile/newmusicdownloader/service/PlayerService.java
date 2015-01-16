db2b9e4085245ad36cc7e63f51686fd59552008c
AndXor <andxor@tut.by>
2015-01-16 13:20:35 +0300

    fix with reset player

--- NewMusicDownloader/src/org/upmobile/newmusicdownloader/service/PlayerService.java
+++ NewMusicDownloader/src/org/upmobile/newmusicdownloader/service/PlayerService.java
@@ -278,7 +278,7 @@ public class PlayerService extends Service implements OnCompletionListener, OnEr
 	public boolean handleMessage(Message msg) {
 		switch (msg.what) {
 		case MSG_START:
-			if (check(SMODE_START_PREPARE) || check(SMODE_START_PREPARE)) {
+			if (check(SMODE_PREPARED) || check(SMODE_START_PREPARE)) {
 				player.reset();
 			}	
 			if (check(SMODE_GET_URL)) {
