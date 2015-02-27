c2b22da35fda5509e938f6d1fd345ab6b97c52ad
AndXor <andxor@tut.by>
2015-02-27 21:26:36 +0300

    small fix

--- NewMusicDownloader/src/org/upmobile/newmusicdownloader/Constants.java
+++ NewMusicDownloader/src/org/upmobile/newmusicdownloader/Constants.java
@@ -9,8 +9,8 @@ public interface Constants {
 	public static final String KEY_SELECTED_SONG = "key.selected.song";
 	public static final String KEY_SELECTED_POSITION = "key.selected.position";
 	
-	public static final String PREF_DIRECTORY_PREFIX = "pref.directory.prefix";
-	public static final String PREF_DIRECTORY = "pref.directory";
+	public static final String PREF_DIRECTORY_PREFIX = "newmusicdownloader.pref.directory.prefix";
+	public static final String PREF_DIRECTORY = "newmusicdownloader.pref.directory";
 	
 	public static final String DIRECTORY_PREFIX = File.separator + "NewMusicDownloader" + File.separator;
 	public static final String DIRECTORY = Environment.getExternalStorageDirectory() + DIRECTORY_PREFIX;
