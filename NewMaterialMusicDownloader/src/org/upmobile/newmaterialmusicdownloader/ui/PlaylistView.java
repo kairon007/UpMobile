137f1f73d5e7c4126ea7577586bc4a76698e6b20
Aleksandr <Aleksandr@192.168.56.1>
2015-03-17 13:19:57 -0300

    add floating button (not finished)

--- NewMaterialMusicDownloader/src/org/upmobile/newmaterialmusicdownloader/ui/PlaylistView.java
+++ NewMaterialMusicDownloader/src/org/upmobile/newmaterialmusicdownloader/ui/PlaylistView.java
@@ -1,20 +1,78 @@
 package org.upmobile.newmaterialmusicdownloader.ui;
 
+import org.upmobile.newmaterialmusicdownloader.R;
 import org.upmobile.newmaterialmusicdownloader.activity.MainActivity;
+import org.upmobile.newmaterialmusicdownloader.application.NewMaterialApp;
 
+import ru.johnlife.lifetoolsmp3.Util;
 import ru.johnlife.lifetoolsmp3.song.MusicData;
 import ru.johnlife.lifetoolsmp3.ui.views.BasePlaylistView;
+import ru.johnlife.lifetoolsmp3.ui.widget.FloatingActionButton;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout.LayoutParams;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.csform.android.uiapptemplate.view.MaterialRippleLayout;
+import com.csform.android.uiapptemplate.view.dlg.MaterialDialog;
+import com.csform.android.uiapptemplate.view.dlg.MaterialDialog.ButtonCallback;
+import com.csform.android.uiapptemplate.view.dlg.Theme;
+
 public class PlaylistView extends BasePlaylistView {
+
+	private ImageView fabIconNew;
+	private FloatingActionButton rightLowerButton;
+	private MaterialDialog.ButtonCallback buttonCallback;
+	private MaterialDialog.Builder builder;
+	private MaterialDialog dialog;
+
 
 	public PlaylistView(LayoutInflater inflater) {
 		super(inflater);
+		buttonCallback = new ButtonCallback() {
+			@Override
+			public void onPositive(MaterialDialog dialog) {
+				super.onPositive(dialog);
+				EditText input = (EditText) dialog.findViewById(android.R.id.edit);
+				String newTitle =  input.getText().toString().trim();
+				if (newTitle.isEmpty()) {
+					dialog.cancel();
+					showMessage(getContext(), R.string.playlist_cannot_be_empty);
+					return;
+				}
+				PlaylistView.this.createPlaylist(getContext().getContentResolver(), input.getText().toString());
+				Util.hideKeyboard(getContext(), input);
+				dialog.cancel();
+			}
+			@Override
+			public void onNegative(MaterialDialog dialog) {
+				super.onNegative(dialog);
+				Util.hideKeyboard(getContext(), dialog.getCustomView());
+				dialog.cancel();
+			}
+		};
+		MaterialRippleLayout layout = new MaterialRippleLayout(getContext());
+        fabIconNew = new ImageView(getContext());
+        fabIconNew.setColorFilter(getResources().getColor(Util.getResIdFromAttribute((Activity) getContext(), R.attr.colorPrimary)));
+        fabIconNew.setImageDrawable(getResources().getDrawable(R.drawable.ic_add_circle_grey));
+        rightLowerButton = new FloatingActionButton.Builder(getContext())
+                .setContentView(fabIconNew, new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT))
+                .build();
+        rightLowerButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				showDialog();
+			}
+		});
 	}
 
 	@Override
@@ -24,12 +82,12 @@ public class PlaylistView extends BasePlaylistView {
 
 	@Override
 	protected Bitmap getDeafultCover() {
-		return null;
+		return ((BitmapDrawable) getResources().getDrawable(R.drawable.ic_album_grey)).getBitmap();
 	}
 
 	@Override
 	protected String getDirectory() {
-		return null;
+		return NewMaterialApp.getDirectory();
 	}
 
 	@Override
@@ -61,5 +119,41 @@ public class PlaylistView extends BasePlaylistView {
 	public void showMessage(Context context, String message) {
 		((MainActivity) context).showMessage(message);
 	}
-
+	
+	@SuppressLint("NewApi")
+	@Override
+	protected void showDialog() {
+		builder = new MaterialDialog.Builder(getContext());
+		builder.theme(Theme.LIGHT)
+		.title(R.string.create_new_playlist)
+		.backgroundColor(getResources().getColor(R.color.main_color_grey_100))
+		.customView(R.layout.md_input_dialog, false)
+		.dividerColorRes(R.color.md_divider_white)
+		.titleColorRes(Util.getResIdFromAttribute((Activity) getContext(), R.attr.colorPrimaryApp))
+		.neutralColorRes(R.color.material_indigo_500)
+		.positiveColorRes(R.color.material_indigo_500)
+		.negativeColorRes(R.color.material_red_500)
+		.callback(buttonCallback)
+		.autoDismiss(false)
+		.positiveText(R.string.create)
+		.negativeText(android.R.string.cancel);
+		dialog = builder.build();
+		dialog.show();
+	}
+	
+	@Override
+	public void closeDialog() {
+		if (null != dialog && dialog.isShowing()) {
+			dialog.cancel();
+		}
+	}
+	
+	public void onResume() {
+		
+	}
+	
+	public void onPause () {
+		
+	}
+	
 }
