90e9d4b6c446ac92c25ed046fe281ddb22dd369f
balamyt <sklep4a@gmail.com>
2015-03-26 09:34:55 -0300

    NewMaterialMusicDownlaoder. Fix UndoBar in PlayerFragment

--- libraries/LifeToolsMp3/src/ru/johnlife/lifetoolsmp3/ui/widget/UndoBarController.java
+++ libraries/LifeToolsMp3/src/ru/johnlife/lifetoolsmp3/ui/widget/UndoBarController.java
@@ -49,7 +49,7 @@ public class UndoBarController extends LinearLayout {
     public static final UndoBarStyle MESSAGESTYLE = new UndoBarStyle(-1, -1, 5000);
 
 
-    private LinkedList<Message> mMessages = new LinkedList();
+    private LinkedList<Message> mMessages = new LinkedList<Message>();
     private Message currentMessage;
     private boolean mShowing;
 
