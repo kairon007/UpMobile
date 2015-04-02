e3ff461da8182ba41bce36c9f67187159d4594f3
AndXor <andxor@tut.by>
2015-04-02 11:50:00 +0300

    small fixed engines

--- libraries/LifeToolsMp3/src/ru/johnlife/lifetoolsmp3/engines/SearchVK.java
+++ libraries/LifeToolsMp3/src/ru/johnlife/lifetoolsmp3/engines/SearchVK.java
@@ -7,7 +7,7 @@ import org.json.JSONObject;
 
 import ru.johnlife.lifetoolsmp3.song.RemoteSong;
 
-public class SearchVK extends SearchWithPages {
+public class SearchVK extends BaseSearchTask {
 	
 	private final String URL = "https://api.vk.com/method/audio.search.json?";
 	
@@ -24,10 +24,6 @@ public class SearchVK extends SearchWithPages {
 
 	@Override
 	protected Void doInBackground(Void... params) {
-		if (page == 1) {
-			maxPages = 100;
-		}
-		if (page > maxPages) return null;
 		try {
 			String songName = URLEncoder.encode(getSongName(), "UTF-8");
 			String link = URL + "&access_token=" + getVkClientToken() + "&q=" + songName + "&count=" + COUNT_SONGS_ON_PAGE;
@@ -36,21 +32,17 @@ public class SearchVK extends SearchWithPages {
 				JSONObject jsonResult = new JSONObject(result);
 				JSONArray mp3list = (JSONArray) jsonResult.getJSONArray("response");
 				int len = mp3list.length();
-				if (len == 0) {
-					maxPages = page - 1;
-				} else {
-					for (int i = 1; i < len; i++) {
-						JSONObject obj = mp3list.getJSONObject(i);
-						String artist = obj.getString(TAG_ARTIST);
-						String title = obj.getString(TAG_TITLE);
-						int duration = obj.getInt(TAG_DURATION);
-						String url = obj.getString(TAG_URL).replace("\\/", "/");
-						RemoteSong song = new RemoteSong(url);
-						song.setArtist(artist);
-						song.setTitle(title);
-						song.setDuration((long) duration * 1000);
-						addSong(song);
-					}
+				for (int i = 1; i < len; i++) {
+					JSONObject obj = mp3list.getJSONObject(i);
+					String artist = obj.getString(TAG_ARTIST);
+					String title = obj.getString(TAG_TITLE);
+					int duration = obj.getInt(TAG_DURATION);
+					String url = obj.getString(TAG_URL).replace("\\/", "/");
+					RemoteSong song = new RemoteSong(url);
+					song.setArtist(artist);
+					song.setTitle(title);
+					song.setDuration((long) duration * 1000);
+					addSong(song);
 				}
 			}
 		} catch (Exception e) {
