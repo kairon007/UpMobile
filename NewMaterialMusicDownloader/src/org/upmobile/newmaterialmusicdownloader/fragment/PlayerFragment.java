ed952efe82db421f6bcc621ed9d0a562d949a92c
balamyt <sklep4a@gmail.com>
2015-04-06 11:42:03 -0300

    Hide checkBox "Use album cover" if song has default cover

--- NewMaterialMusicDownloader/src/org/upmobile/newmaterialmusicdownloader/fragment/PlayerFragment.java
+++ NewMaterialMusicDownloader/src/org/upmobile/newmaterialmusicdownloader/fragment/PlayerFragment.java
@@ -748,6 +748,7 @@ public class PlayerFragment extends Fragment implements Constants, OnClickListen
 	private void getCover(final AbstractSong s) {
 		setCheckBoxState(false);
 		setCoverToZoomView(null);
+		((View)cbUseCover.getParent()).setVisibility(View.GONE);
 		if (s.getClass() != MusicData.class) {
 			OnBitmapReadyListener readyListener = new OnBitmapReadyListener() {
 
@@ -757,6 +758,7 @@ public class PlayerFragment extends Fragment implements Constants, OnClickListen
 						return;
 					}
 					if (null != bmp) {
+						((View)cbUseCover.getParent()).setVisibility(View.VISIBLE);
 						((RemoteSong) s).setHasCover(true);
 						setCoverToZoomView(bmp);
 						player.updatePictureNotification(bmp);
@@ -769,6 +771,7 @@ public class PlayerFragment extends Fragment implements Constants, OnClickListen
 		} else {
 			Bitmap bitmap = ((MusicData) s).getCover(getActivity());
 			if (bitmap != null) {
+				((View)cbUseCover.getParent()).setVisibility(View.VISIBLE);
 				setCoverToZoomView(bitmap);
 				setCheckBoxState(true);
 			} 
@@ -778,6 +781,7 @@ public class PlayerFragment extends Fragment implements Constants, OnClickListen
 	private void clearCover() {
 		setCheckBoxState(false);
 		if (MusicData.class == song.getClass()) {
+			((View)cbUseCover.getParent()).setVisibility(View.GONE);
 			setCoverToZoomView(null);
 			((MusicData) song).clearCover();
 			RenameTask.deleteCoverFromFile(new File(song.getPath()));
