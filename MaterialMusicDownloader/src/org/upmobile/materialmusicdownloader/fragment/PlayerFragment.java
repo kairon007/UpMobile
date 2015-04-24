672d369a2c9ec0ede8010932b238f92be9dab3f5
Sashka <bolshakov.aleksander@gmail.com>
2015-04-24 11:31:05 +0300

    fix #826

--- MaterialMusicDownloader/src/org/upmobile/materialmusicdownloader/fragment/PlayerFragment.java
+++ MaterialMusicDownloader/src/org/upmobile/materialmusicdownloader/fragment/PlayerFragment.java
@@ -222,10 +222,10 @@ public class PlayerFragment extends Fragment implements OnClickListener, BaseMat
 		@Override
 		public void onCancelled() {
 			canceled = true;
-			((RippleView) download.getParent()).setEnabled(true);
-			download.setOnClickListener(PlayerFragment.this);
-			download.setIndeterminateProgressMode(false);
 			download.setProgress(CircularProgressButton.IDLE_STATE_PROGRESS);
+			download.setOnClickListener(PlayerFragment.this);
+			setDownloadButtonState(true);
+			((RippleView) download.getParent()).setEnabled(true);
 		}
 
 		@Override
